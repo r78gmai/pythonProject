38fc5fe977cd9cbf0411259c7a49c16807ae7f98
Lii78 <r777777778888@yahoo.co.jp>
2022-08-03 08:37:22 +0900

    Changes

--- lchika.py
+++ lchika.py
@@ -6,10 +6,11 @@ GPIO.setup(20,GPIO.OUT) #green
 GPIO.setup(22,GPIO.OUT) #red
 
 for x in range(10):
-    GPIO.output(20,0)
-    GPIO.output(22,1)
-    time.sleep(0.5)
-    GPIO.output(20,1)
-    GPIO.output(22,0)
-    time.sleep(0.5)
+    GPIO.output(20,0) #green off
+    GPIO.output(22,1) #red on
+    time.sleep(2)   #↑ time (red 2s light)
+    GPIO.output(20,1) #green on
+    GPIO.output(22,0) #red off
+    time.sleep(0.5)  #↑ time (green 0.5s light)
+
 GPIO.cleanup()
