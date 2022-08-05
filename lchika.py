bdabdde25c7dd991532de44c236b69760f88faed
Lii78 <r777777778888@yahoo.co.jp>
2022-08-05 09:45:32 +0900

    Changes
    delete comment

--- lchika.py
+++ lchika.py
@@ -3,27 +3,27 @@ import time
 
 GREEN_LED = 20
 RED_LED = 22
-LED_ON_OFF_FLAG = 0
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup( GREEN_LED ,GPIO.OUT) #green
-GPIO.setup(22,GPIO.OUT) #red
+GPIO.setup( GREEN_LED , GPIO.OUT)       # green
+GPIO.setup( RED_LED , GPIO.OUT)         # red
 
 #beat
 for x in range (3):
     GPIO.output(GREEN_LED, 1)  # green on
-    time.sleep(0.2)  # ↑ time (red 2s light)
+    time.sleep(0.2)  # 0.2s
     GPIO.output(GREEN_LED, 0)  # green off
-    time.sleep(0.2)  # ↑ time (red 2s light)
-    GPIO.output(GREEN_LED, 1)  # green on
-    time.sleep(0.2)  # ↑ time (red 2s light)
-    GPIO.output(GREEN_LED, 0)  # green off
-    time.sleep(0.2)  # ↑ time (red 2s light)
+    time.sleep(0.2)
+    GPIO.output(GREEN_LED, 1)
+    time.sleep(0.2)
+    GPIO.output(GREEN_LED, 0)
+    time.sleep(0.2)
     GPIO.output(22, 1)  # red on
-    time.sleep(0.5)  # ↑ time (red 2s light)
+    time.sleep(0.5)     # 0.5
     GPIO.output(22, 0)  # red off
     time.sleep(0.5)
 
+LED_ON_OFF_FLAG = 0
 # for x in range(5):
 #     #led_flag = 0
 #     if ( LED_ON_OFF_FLAG ):
