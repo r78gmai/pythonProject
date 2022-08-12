c62f8f243ef58ce74e3431bea9ca3f2f0767481a
Lii78 <r777777778888@yahoo.co.jp>
2022-08-12 18:41:16 +0900

    Changes
    first commit

---
+++ oneday_programing.py
@@ -0,0 +1,15 @@
+
+
+for x in range(9, 11):
+    print (x)
+
+a = 20
+if a > 10 and a <= 20:
+    print('aは１１から２０までの数字')
+if 10 < a <= 20:
+    print('aは１１から２０までの数字')
+if a == 10 or a <= 20:
+    print('aは１0もしくは２０以下の数字')
+if not a == 10:
+    print('aは１0以外の数字')
+
